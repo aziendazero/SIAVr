52d1889d1d8494caa173528637a74db3a25cf2fe
mchiadini <83723042+mchiadini@users.noreply.github.com>
2021-05-10 09:13:14 +0200

    Add Tag folder and his content

---
+++ OnVac.ObjectModel/Entities/Tag/Tag.vb
@@ -0,0 +1,21 @@
+﻿Namespace Entities
+    Public Class Tag
+        Public Property Id As Long
+        Public Property Descrizione As String
+        Public Property Gruppo As String
+        Public Property Colore As String
+    End Class
+
+    Public Class TagTmp
+        Public Property Id As String
+        Public Property Descrizione As String
+        Public Property Gruppo As String
+        Public Property Colore As String
+    End Class
+
+    <Serializable()>
+    Public Class ExportTag
+        Public Property IdEpisodio As Long
+        Public Property Descrizione As String
+    End Class
+End Namespace
