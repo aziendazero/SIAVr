2ef75054bbf5c8a72fca4dfa66af140860ef7a69
mchiadini <83723042+mchiadini@users.noreply.github.com>
2021-05-10 08:46:53 +0200

    Add OnVac.Log.Manager

---
+++ OnVac.Log.Manager/PazienteLogManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Onit.OnAssistnet.OnVac.Log.Manager
+{
+    public class PazienteLogManager
+    {
+        publ
+    }
+}
---
+++ OnVac.Log.Manager/Properties/AssemblyInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OnVac.Log.Manager")]
+
+// Compile a Debug or Release flag into the assembly.
+#if DEBUG
+[assembly: AssemblyDescription("Debug Version")]
+#else
+[assembly: AssemblyDescription("Release Version")]
+#endif
+
+//
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Numero di versione principale
+//      Numero di versione secondario 
+//      Numero revisione
+//      Numero build
+//
+// È possibile specificare tutti i valori o impostare come predefiniti i valori Numero revisione e Numero build 
+// utilizzando l'asterisco (*) come illustrato di seguito:
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OnVac.Log.Manager")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9251ae9f-280a-428e-b390-c7813ae84af8")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Onit.OnAssistnet.OnVac.Constants.Version)]
+
+
